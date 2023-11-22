6ffd4b49b56afa1a46f3f00a2c5a9174478ccb47
justfoxx <kittech@skiff.com>
2023-11-22 22:36:55 +0100

    Added Place struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::fmt::{Display, Formatter};
+use std::fmt::{Display, Error, format, Formatter};
+use std::io::{stdout, Write};
 use colored::{Color, Colorize};
 
 #[derive(PartialEq, Eq, Clone)]
@@ -42,58 +43,65 @@ impl Display for Piece {
     }
 }
 
-enum Move {
-    Up(u8),
-    Down(u8),
-    Left(u8),
-    Right(u8),
-    UpLeft(u8),
-    UpRight(u8),
-    DownLeft(u8),
-    DownRight(u8),
-}
-
 type Board = [[Piece;8];8];
 
+#[derive(PartialEq, Eq)]
+struct Place {
+    x: usize,
+    y: usize,
+}
+
+impl Place {
+    fn new(x: usize, y: usize) -> Place {
+        Place {
+            x,
+            y
+        }
+    }
+}
+
 trait BoardTrait {
-    fn print_board(&self);
-    fn move_piece_and_print(&mut self, place: (usize, usize), moved: Move) -> Result<(), &str>;
+    fn print_board(&self) -> Result<(), std::io::Error>;
+    fn move_piece_and_print(&mut self, place: Place, moved: Place) -> Result<(), &str>;
 }
 
 impl BoardTrait for Board {
-    fn print_board(&self) {
+    fn print_board(&self) -> Result<(), std::io::Error> {
+        let mut lock = stdout().lock();
         for (i, row) in self.iter().enumerate() {
             for (j, cell) in row.iter().enumerate() {
                 let colored_cell = match (i + j) % 2 == 0 {
                     true => cell.to_string().on_white(),
                     false => cell.to_string().on_black(),
                 };
-                print!("{}", colored_cell);
+                lock.write_all(colored_cell.to_string().as_bytes())?;
             }
-            println!();
+            lock.write_all("\n".as_bytes())?;
+            lock.flush()?;
         }
+        Ok(())
     }
 
-    fn move_piece_and_print(&mut self, place: (usize, usize), moved: Move) -> Result<(), &str> {
-        let piece = self[place.0][place.1].clone();
+    fn move_piece_and_print(&mut self, from: Place, to: Place) -> Result<(), &str> {
+        if to == from && to.x >= self.len() && to.y >= self.len() && from.x >= self.len() && from.y >= self.len() {
+            return Err("Invalid move")
+        }
+        let coords = (
+            (self.len()-1-from.y, from.x),
+            (self.len()-1-to.y, to.x)
+        );
+        let piece = self[coords.0.0][coords.0.1].clone();
         match piece {
             Piece::Empty => return Err("No piece to move"),
             _ => {
-                match moved {
-                    Move::Up(i) => self[place.0 + i as usize][place.1] = piece,
-                    Move::Down(i) => self[place.0 - i as usize][place.1] = piece,
-                    Move::Left(i) => self[place.0][place.1 + i as usize] = piece,
-                    Move::Right(i) => self[place.0][place.1 - i as usize] = piece,
-                    Move::UpLeft(i) => self[place.0 + i as usize][place.1 - i as usize] = piece,
-                    Move::UpRight(i) => self[place.0 + i as usize][place.1 + i as usize] = piece,
-                    Move::DownLeft(i) => self[place.0 - i as usize][place.1 - i as usize] = piece,
-                    Move::DownRight(i) => self[place.0 - i as usize][place.1 + i as usize] = piece,
-                }
-                self[place.0][place.1] = Piece::Empty;
+                self[coords.1.0][coords.1.1] = piece;
+                self[coords.0.0][coords.0.1] = Piece::Empty;
             }
         }
-        self.print_board();
-        Ok(())
+        match self.print_board() {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Failed to print board"),
+        }
     }
 
 }
@@ -111,6 +119,7 @@ const CHESS_BOARD: Board = [
 
 fn main() {
     let mut chess_board = CHESS_BOARD;
-    chess_board.print_board();
-    chess_board.move_piece_and_print((0,0), Move::Up(2)).unwrap();
+    chess_board.print_board().unwrap();
+    chess_board.move_piece_and_print(Place::new(0,0), Place::new(0,2)).unwrap();
+    chess_board.move_piece_and_print(Place::new(0,2), Place::new(1,4)).unwrap();
 }
